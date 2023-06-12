bc1d50ea6bd5ef8cf93368e7d5a38cf597d44c32
davidherman1 <115696296+davidherman1@users.noreply.github.com>
2023-06-12 10:51:55 +0200

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,33 @@
+let data = []
+let recentData = []
+let whiteLine = 0
+const pinC = DigitalPin.P15
+const pinL = DigitalPin.P14
+const pinR = DigitalPin.P13
+function carMotor(ul: number = 0, ur: number = 0, ll: number = 0, lr: number = 0) {
+    ul = Math.map(ll, -100, 100, -255, 255)
+    ur = Math.map(lr, -100, 100, -255, 255)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, -ul)
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, ur)
+}
+pins.setPull(pinC, PinPullMode.PullNone)
+pins.setPull(pinL, PinPullMode.PullNone)
+pins.setPull(pinR, PinPullMode.PullNone)
+// true == on black line
+// false == not on black line
+basic.forever(function () {
+    let c = (whiteLine ^ pins.digitalReadPin(pinC)) == 0 ? false : true
+    let l = (whiteLine ^ pins.digitalReadPin(pinL)) == 0 ? false : true
+    let r = (whiteLine ^ pins.digitalReadPin(pinR)) == 0 ? false : true
+    if (c === true && l === true && r === true || c === true && l === false && r === false) {
+        carMotor(0, 0, -70, -70)
+    } else if (r === false && c === true && l === true || c === false && r === false && l === true) {
+        carMotor(0, 0, -70, 0)
+    } else if (r === true && c === true && l === false || c === false && r === true && l === false) {
+        carMotor(0, 0, 0, -70)
+    }
+    else {
+        
+        carMotor(0, 0, -50, -50)
+    }
+})
